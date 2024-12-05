928c8ff441663b73de09a62cbd2368785197461d
behruz zoirov <behruzzoirov@behruzs-MacBook-Air.local>
2024-12-05 20:53:09 +0500

    refactoring and upgraid

--- src/trace.rs
+++ src/trace.rs
@@ -1,12 +1,19 @@
-use winterfell::TraceTable;
+use winterfell::math::{FieldElement, StarkField};
+use winterfell::{Trace, TraceTable};
 use winterfell::math::fields::f128::BaseElement;
-use crate::utils::extract_timestamp;
+use crate::utils::{calculate_distance, extract_timestamp};
 
-// Функция для создания трассировки из данных GPX
+
+
 pub fn build_gps_trace_from_gpx(gpx: &gpx::Gpx) -> TraceTable<BaseElement> {
-    let trace_width: usize = 3; // Ширина трассировки: широта, долгота, время
-    let n = gpx.waypoints.len(); // Количество точек в маршруте
-    let mut trace: TraceTable<BaseElement> = TraceTable::new(trace_width, n);
+    let mut current_len = gpx.waypoints.len();
+    if !current_len.is_power_of_two() {
+        let next = current_len.next_power_of_two();
+        println!("Current length is: {}. Next power of 2 is: {}", current_len, next);
+        current_len = next;
+    }
+    let trace_width: usize = 4; // Ширина трассировки: широта, долгота, время
+    let mut trace: TraceTable<BaseElement> = TraceTable::new(trace_width, current_len);
 
     let scale_factor: f64 = 10_000_000.0; // Коэффициент масштабирования для координат
 
@@ -16,21 +23,54 @@ pub fn build_gps_trace_from_gpx(gpx: &gpx::Gpx) -> TraceTable<BaseElement> {
             state[0] = BaseElement::new((first_point.point().y() * scale_factor) as u128);
             state[1] = BaseElement::new((first_point.point().x() * scale_factor) as u128);
             state[2] = BaseElement::new(extract_timestamp(first_point.time) as u128);
-         },
+            state[3] = BaseElement::ZERO;
+        },
         |step, state: &mut [BaseElement]| {
             if step < gpx.waypoints.len() - 1 {
                 let next_point = &gpx.waypoints[step + 1];
                 let next_lat_u128 = (next_point.point().y() * scale_factor) as u128;
                 let next_lon_u128 = (next_point.point().x() * scale_factor) as u128;
                 let next_time = extract_timestamp(next_point.time) as u128;
+
+                let previous_timestamp = state[2].as_int() as u128;
+                let time_diff = next_time - previous_timestamp;
                 state[0] = BaseElement::new(next_lat_u128);
                 state[1] = BaseElement::new(next_lon_u128);
                 state[2] = BaseElement::new(next_time);
+                state[3] = BaseElement::new(time_diff);
             }
         },
     );
-    
 
     trace
 }
 
+pub fn display_trace(trace: &TraceTable<BaseElement>) {
+    println!("Трассировка:");
+    let n = trace.length();
+
+    for i in 0..n {
+        let lat = trace.get(0, i).as_int() as f64 / 10_000_000.0;
+        let lon = trace.get(1, i).as_int() as f64 / 10_000_000.0;
+        let time = trace.get(2, i).as_int(); 
+
+        if i < n - 1 {
+            let next_time = trace.get(2, i + 1).as_int();
+            let next_lat = trace.get(0, i + 1).as_int() as f64 / 10_000_000.0;
+            let next_lon = trace.get(1, i + 1).as_int() as f64 / 10_000_000.0;
+
+            let time_diff = (next_time - time) as u128; // Разница времени
+            let distance = calculate_distance(lat, lon, next_lat, next_lon);
+
+            println!(
+                "Шаг {}: Широта: {:.7}, Долгота: {:.7}, Расстояние до след.: {:.2} м, Время: {} сек, Разница времени: {} сек, ",
+                i, lat, lon, distance, time, time_diff, 
+            );
+        } else {
+            println!(
+                "Шаг {}: Широта: {:.7}, Долгота: {:.7}, Время: {} сек, ",
+                i, lat, lon, time, 
+            );
+        }
+    }
+}
--- src/utils.rs
+++ src/utils.rs
@@ -5,30 +5,30 @@ use std::f64::consts::PI;
 pub fn extract_timestamp(time: Option<gpx::Time>) -> i64 {
     match time {
         Some(t) => {
-            let offset_datetime: OffsetDateTime = t.into(); // Преобразуем в OffsetDateTime
+            let offset_datetime: OffsetDateTime = t.into(); 
 
-            // Используем unwrap() для извлечения значения из Option
+         
             let chrono_time: DateTime<Utc> = DateTime::from_timestamp(offset_datetime.unix_timestamp(),
              offset_datetime.nanosecond())
                 .expect("Failed to convert OffsetDateTime to DateTime<Utc>");
 
-            chrono_time.timestamp() // Получаем timestamp
+            chrono_time.timestamp() 
         },
-        None => 0, // Если времени нет, возвращаем 0
+        None => 0, 
     }
 }
 
-// Вычисление расстояния между координатами
-pub fn calculate_distance(lat1: f64, lon1: f64, lat2: f64, lon2: f64) -> f64 {
-    let r = 6371_000.0; // Радиус Земли в метрах
+
+pub fn calculate_distance(lat: f64, lon: f64, next_lat: f64, next_lon: f64) -> f64 {
+    let r = 6371_000.0; 
     let to_rad = |deg: f64| deg * PI / 180.0;
 
-    let dlat = to_rad(lat2 - lat1);
-    let dlon = to_rad(lon2 - lon1);
+    let dlat = to_rad(next_lat - lat);
+    let dlon = to_rad(next_lon - lon);
     let a = (dlat / 2.0).sin().powi(2)
-        + lat1.to_radians().cos() * lat2.to_radians().cos() * (dlon / 2.0).sin().powi(2);
+        + lat.to_radians().cos() * next_lat.to_radians().cos() * (dlon / 2.0).sin().powi(2);
     let c = 2.0 * a.sqrt().atan2((1.0 - a).sqrt());
 
-    r * c // Расстояние в метрах
+    r * c 
 }
 
